02098f799816d03de18035e73df868ce80b3dfbf
mattmcgiv <matt@antym.com>
2016-01-10 10:57:56 -0700

    Add a couple more jokes and randomize them.

--- jokes/src/main/java/com/antym/JokeProvider.java
+++ jokes/src/main/java/com/antym/JokeProvider.java
@@ -1,7 +1,20 @@
 package com.antym;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class JokeProvider {
-    public String getJoke() {
-        return "A skeleton walks into a bar and says, \"Gimme a beer and a mop.\"";
+
+    public static String getJoke() {
+        ArrayList<String> jokes = new ArrayList<String>();
+        jokes.add("A skeleton walks into a bar and says, \"Gimme a beer and a mop.\"");
+        jokes.add("Why didn't the skeleton go to the party?\n" +
+                "He had no body to go with.\n");
+        jokes.add("Q: Why wouldn't the skeleton jump off the cliff?\n" +
+                "A: Because he didn't have the guts.");
+
+        Random random = new Random();
+        return jokes.get(random.nextInt(jokes.size()));
+
     }
 }
