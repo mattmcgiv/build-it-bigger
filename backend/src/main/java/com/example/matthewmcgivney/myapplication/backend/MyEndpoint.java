bf4cf7c655dbf7c427883a9404b9c13307db524b
mattmcgiv <matt@antym.com>
2016-01-10 19:17:10 -0700

    Make GCE serve up jokes to the activity.

--- backend/src/main/java/com/example/matthewmcgivney/myapplication/backend/MyEndpoint.java
+++ backend/src/main/java/com/example/matthewmcgivney/myapplication/backend/MyEndpoint.java
@@ -6,6 +6,7 @@
 
 package com.example.matthewmcgivney.myapplication.backend;
 
+import com.antym.JokeProvider;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -28,7 +29,9 @@ public class MyEndpoint {
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        JokeProvider jp = new JokeProvider();
+        String joke = jp.getJoke();
+        response.setData("Hi, " + joke);
 
         return response;
     }
